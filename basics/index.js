4a1770f1ac45cfd618818b023369ef0065dd7dab
ganerdene1797 <93957706+ganerdene1797@users.noreply.github.com>
2021-11-25 10:16:46 +0800

    shalgalt

--- basics/index.js
+++ basics/index.js
@@ -1,39 +1,99 @@
+/*
+
 // 1. 0 - ээс 100 хүртэлх тоог хэвлэ.
-// -- Энд кодоо бичнэ үү!!
+
+for(let i=0; i<=100; i++) {
+    console.log(i);
+} 
 
 
 // 2. 60 - с 30 хүртэлх бүх СОНДГОЙ тоог хэвлэ.
-// -- Энд кодоо бичнэ үү!!
+let z = 60;
+while( z>=30 ) {
+    if ((z+1)%2===0) console.log(z); 
+    z--;
+} 
 
 // 3. Чагтаас бүрдсэн квадрат зурах ба баруун тийшээ налсан диагональ нь нэмэх тэмдэг байна:
 // ######+
 // #####+#
-// ####+##
+// ####+## 
 // ###+###
 // ##+####
 // #+#####
-// +######
-// -- Энд кодоо бичнэ үү!!
+// +###### 
+
+let n = parseInt( prompt("Kwadratiin taliig oruul:") ) 
+
+for(let i=0; i<n; i++) { 
+    let k = 1; 
+    for(let j=0; j<n-k; j++) {
+        console.log('#')
+    }
+
+    console.log('+')
+
+    for(let t=1; t<k; t++) {
+        console.log('#') 
+    } 
+    console.log('\n') 
+    k++; 
+}
+
 
 // 4. Регистрийн дугаар өгөхөд төрсөн он, сар, өдөр (YYYY-MM-DD) хүйсийг хэвлэдэг convertRegNum функц бич.
 //      РД - гийн сүүлийн орон сондгой бол эрэгтэй, тэгш бол эмэгтэй байна.
 // Жишээ нь:
 //      convertRegNum('АБ90020321')
 //      >> 1990-02-03 онд төрсөн эрэгтэй
-// -- Энд кодоо бичнэ үү!!
+
+
+function convertRegNum(register) {
+    let arr = register.split(''); 
+    let birth = arr.slice(2, 4).join('') + '-' + arr.slice(4, 6).join('') + '-' + arr.slice(6, 8).join(''); 
+
+    if(arr[arr.length-1] % 2 === 0) {
+        console.log(birth, ' tursun ', 'emegtei')
+    } else 
+    console.log(birth, ' tursun ', 'eregtei')
+
+} 
+
+*/ 
+
 
 // 5. Бүх .red-boxes гэсэн класстай элемэнтийг улаан болго;
-// -- Энд кодоо бичнэ үү!!
+
+let $array = document.querySelectorAll('.red-boxes'); 
+
+for(let i = 0; i < $array.length; i++) {
+    $array[i].style.color = "red"; 
+} 
 
 // 6. blue-box гэсэн ID-тай элемэнтийг цэнхэр болго;
-// -- Энд кодоо бичнэ үү!!
 
-// 7. Бүх div - ийг бор болго;
-// -- Энд кодоо бичнэ үү!!
+// document.querySelector('#blue-box').style.color = "blue"; 
 
-// 8. Шинээр box гэсэн класстай section tag үүсгээд, хар өнгөтэй болгоно.
+// 7. Бүх div - ийг бор болго; 
+
+let $divs = document.querySelectorAll('div'); 
+
+for(let i = 0; i < $divs.length; i++) {
+    $divs[i].style.color = "brown";  
+} 
+
+// 8. Шинээр box гэсэн класстай section tag үүсгээд, хар өнгөтэй болгоно. 
 //    Дотор нь box гэсэн класстай article tag үүсгэнэ.
 //    article tag - ыг улаан өнгөтэй бөөрөнхий болгоно (JS - ээр)
-//    section tag - ыг body руу append хийнэ.
-//    улаан бөөрөнхий дээр дарахад бөөрөнхийг ногоон болгоно.
-// -- Энд кодоо бичнэ үү!!
+//    section tag - ыг body руу append хийнэ. 
+//    улаан бөөрөнхий дээр дарахад бөөрөнхийг ногоон болгоно. 
+
+const $section = document.createElement('section'); 
+$section.style.color = "black"; 
+
+let $article = document.createElement('article'); 
+$article.className = "box";  
+
+
+
+
--- datatypes/index.js
+++ datatypes/index.js
@@ -1,20 +1,34 @@
 const string = 'apple, banana, kiwi';
 
-// 1. Өгсөн string - ийн бүх үсгийг том болго: 'APPLE, BANANA, KIWI'
-// -- Энд кодоо бичнэ үү!
+// 1. Өгсөн string - ийн бүх үсгийг том болго: 'APPLE, BANANA, KIWI' 
 
-// 2. Өгсөн string - ийг дараах хэлбэртэй болго: 'Apple; Banana; Kiwi'
-// -- Энд кодоо бичнэ үү!
+let upper = string.toUpperCase();
+ 
+// 2. Өгсөн string - ийг дараах хэлбэртэй болго: 'Apple; Banana; Kiwi' 
+
+let arr = string.split(", ")
+let result = arr.join("; ")
+arr[0][0] = arr[0].charAt(0).toUpperCase(); 
+arr[1][0] = arr[1].charAt(0).toUpperCase(); 
+arr[2][0] = arr[2].charAt(0).toUpperCase();  
 
 const array = ['d', 'e', 'c', 'a', 'b'];
-// 3. Өгсөн array - ыг үсгийн дарааллаар эрэмблэ.
-// -- Энд кодоо бичнэ үү!
+// 3. Өгсөн array - ыг үсгийн дарааллаар эрэмблэ. 
+
+array.sort();
 
 // 4. Өгсөн array - ын голын 3н элемэнтийг салгаж аваад array1 - гэсэн хувьсагчид хийнэ.
-//    array болон array1 ийг хэвлэ.
-// -- Энд кодоо бичнэ үү!
+//    array болон array1 ийг хэвлэ. 
 
-// 5. Өгсөн array - ын эхний элемэнтийг устага
+const middle = parseInt(array.length / 2); 
+let array1 = [];
+array1[0] = array[middle - 1];
+array1[1] = array[middle];
+array1[2] = array[middle + 1];
+ 
+
+// 5. Өгсөн array - ын эхний элемэнтийг устага 
+
 // -- Энд кодоо бичнэ үү!
 
 // 6. Өгсөн array - ын сүүлийн элемэнтийг устага
